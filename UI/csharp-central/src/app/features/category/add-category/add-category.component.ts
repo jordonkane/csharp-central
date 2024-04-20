7d9f6a71d2678e48bb8202ecfb8b5061779d2ce5
jordonkane <kanejord99@outlook.com>
2024-04-20 10:28:24 -0400

    Implement angular services

--- UI/csharp-central/src/app/features/category/add-category/add-category.component.ts
+++ UI/csharp-central/src/app/features/category/add-category/add-category.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
 
   model: AddCategoryRequest;
+  private addCategorySubscription?: Subscription;
 
-  constructor(){
+  constructor(private categoryService: CategoryService){
     this.model = {
       name: '',
       urlHandle: ''
@@ -19,6 +22,15 @@ export class AddCategoryComponent {
 
   // Capture the two values of the forms and submit it to the API
   onFormSubmit(){
-    console.log(this.model);
+    this.addCategorySubscription = this.categoryService.addCategory(this.model)
+    .subscribe({
+      next: (response) => {
+        console.log('This was successful!');
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
   }
 }
